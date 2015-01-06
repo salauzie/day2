87b5894084ff5a6611e763aeb91d6722742669b3
Stephanie Lauziere <salauzie@gmail.com>
2015-01-06 12:05:30 -0500

    first commit

---
+++ day2notes.rb
@@ -0,0 +1,15 @@
+# [] - brackets
+# {} - curly brackets
+array1 = [1,2,3,4 "string"]
+puts array1.inspect
+puts array1[0].inspect
+puts array1[4].inspect
+
+students = ["jesse", "adam", "jackie", "troy", "matt", "ryan"]
+puts students[2].inspect
+students.push("robbi")
+puts students.inspect
+students.push("steph", "will")
+puts students.inspect
+
+students = []
